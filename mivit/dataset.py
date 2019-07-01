f5781e18f6a041e19909c6139346b363771db565
Leslie Lamarche <leslie.lamarche@sri.com>
2019-07-01 08:25:31 -0700

    Removed coord_convert.py.

--- mivit/dataset.py
+++ mivit/dataset.py
@@ -1,10 +1,7 @@
 # dataset.py
 
 import numpy as np
-from . import coord_convert as cc
 import pymap3d as pm
-
-# No vector conversion in pymap3d?
 
 class DataSet(object):
     def __init__(self,**kwargs):
@@ -44,7 +41,7 @@ class DataSet(object):
             pass
         elif all(k in kwargs for k in ('site','azimuth','elevation','slantrange')):
             # kwargs['latitude'], kwargs['longitude'], kwargs['altitude'] = self.azel2gd_ranges(kwargs['site'][0],kwargs['site'][1],kwargs['site'][2],kwargs['azimuth'],kwargs['elevation'],kwargs['ranges'])
-            lat, lon, alt = pm.aer2geodetic(kwargs['azimuth'], kwargs['elevation'], kwargs['range'], kwargs['site'][0],kwargs['site'][1],kwargs['site'][2])
+            kwargs['latitude'], kwargs['longitude'], kwargs['altitude'] = pm.aer2geodetic(kwargs['azimuth'], kwargs['elevation'], kwargs['range'], kwargs['site'][0],kwargs['site'][1],kwargs['site'][2])
         elif all(k in kwargs for k in ('site','azimuth','elevation','altitude')):
             # kwargs['latitude'], kwargs['longitude'], kwargs['altitude'] = self.azel2gd_alt(kwargs['site'][0],kwargs['site'][1],kwargs['site'][2],kwargs['azimuth'],kwargs['elevation'],kwargs['altitude'])
             kwargs['latitude'], kwargs['longitude'], kwargs['altitude'] = self.projalt(kwargs['site'], kwargs['azimuth'], kwargs['elevation'], kwargs['altitude'])
@@ -65,13 +62,12 @@ class DataSet(object):
         Convert vectors with satellite forward, left, up components to geodetic East, North, Up
         '''
 
-        # x, y, z = cc.geodetic_to_cartesian(latitude, longitude, altitude)
         x, y, z = pm.geodetic2ecef(latitude, longitude, altitude)
 
         sat_position = np.array([x, y, z]).T
         sat_pos_dif = np.concatenate((np.array([np.nan,np.nan,np.nan])[None,:],sat_position[2:,:]-sat_position[:-2,:],np.array([np.nan,np.nan,np.nan])[None,:]),axis=0)
         forwECEF = sat_pos_dif/np.linalg.norm(sat_pos_dif,axis=-1)[:,None]
-        # vn, ve, vz = cc.vector_cartesian_to_geodetic(forwECEF[:,0], forwECEF[:,1], forwECEF[:,2], x, y, z)
+
         ve, vn, vz = pm.uvw2enu(forwECEF[:,0], forwECEF[:,1], forwECEF[:,2], latitude, longitude)
 
         forw = np.array([ve, vn, vz]).T
@@ -86,48 +82,17 @@ class DataSet(object):
 
 
 
-    # def azel2gd_ranges(self,lat0,lon0,alt0,az,el,ranges):
-    #     # convert azimuth and elevation to geodetic coordinates given ranges
-    #     ranges = ranges*1000.
-    #     el = el*np.pi/180.
-    #     az = az*np.pi/180.
-    #     # x0, y0, z0 = cc.geodetic_to_cartesian(lat0,lon0,alt0)
-    #     x0, y0, z0 = pm.geodetic2ecef(lat0, lon0, alt0)
-
-    #     ve = np.cos(el)*np.sin(az)
-    #     vn = np.cos(el)*np.cos(az)
-    #     vu = np.sin(el)
-
-    #     vx, vy, vz = cc.vector_geodetic_to_cartesian(vn,ve,vu,lat0,lon0,alt0)
-
-    #     # lat, lon, alt = cc.cartesian_to_geodetic(x0+vx*ranges,y0+vy*ranges,z0+vz*ranges)
-    #     lat, lon, alt = pm.ecef2geodetic(x0+vx*ranges,y0+vy*ranges,z0+vz*ranges)
-
-    #     return lat, lon, alt
-
-    # def azel2gd_alt(self,lat0,lon0,alt0,azimuth,elevation,proj_alt):
     def projalt(self,site,azimuth,elevation,proj_alt):
         # convert azimuth and elevation to geodetic coordinates given a projection altitude
-        # azimuth = azimuth*np.pi/180.
-        # elevation = elevation*np.pi/180.
+
         points = np.arange(0.,max(proj_alt)/np.sin(min(elevation)),1.)*1000.
-
-        # # x0, y0, z0 = cc.geodetic_to_cartesian(lat0,lon0,alt0)
-        # x0, y0, z0 = pm.geodetic2ecef(lat0,lon0,alt0)
-
-        # ve = np.cos(elevation)*np.sin(azimuth)
-        # vn = np.cos(elevation)*np.cos(azimuth)
-        # vu = np.sin(elevation)
-        # vx, vy, vz = cc.vector_geodetic_to_cartesian(vn,ve,vu,lat0,lon0,alt0)
-        # # lat, lon, alt = cc.cartesian_to_geodetic(x0+vx*points[:,None],y0+vy*points[:,None],z0+vz*points[:,None])
-        # lat, lon, alt = pm.ecef2geodetic(x0+vx*points[:,None],y0+vy*points[:,None],z0+vz*points[:,None])
-
 
         lat, lon, alt = pm.aer2geodetic(azimuth, elevation, points[:,None], site[0], site[1], site[2])
 
         # find index closeset to the projection altitude
-        idx = np.argmin(np.abs(alt-proj_alt),axis=0)
+        idx = np.argmin(np.abs(alt-proj_alt*1000.),axis=0)
 
+        # select altitude at correct index for each beam (fancy indexing with tuples to avoid for loops)
         latitude = lat[tuple(idx),tuple(np.arange(len(proj_alt)))]
         longitude = lon[tuple(idx),tuple(np.arange(len(proj_alt)))]
         altitude = alt[tuple(idx),tuple(np.arange(len(proj_alt)))]
